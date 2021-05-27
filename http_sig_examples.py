d8e713f81cd0baaac42cb5d6f7ec8f5871f4577f
Justin Richer <justin@bspk.io>
2021-05-27 16:13:31 -0400

    ported from m2crypto to pycryptodomex

--- http_sig_examples.py
+++ http_sig_examples.py
@@ -1,8 +1,16 @@
 #!/usr/bin/env python
 
 import http_sfv
-import M2Crypto
-import hashlib
+from Cryptodome.Signature import pss
+from Cryptodome.Signature import pkcs1_15
+from Cryptodome.Hash import SHA512
+from Cryptodome.Hash import SHA256
+from Cryptodome.PublicKey import RSA
+from Cryptodome import Random
+from Cryptodome.IO import PEM
+from Cryptodome.IO import PKCS8
+from Cryptodome.Signature.pss import MGF1
+mgf512 = lambda x, y: MGF1(x, y, SHA512)
 
 rsaTestKey = b"""
 -----BEGIN RSA PUBLIC KEY-----
@@ -43,8 +51,7 @@ EQeNC8fHGg4UXU8mhHnSBt3EA10qQJfRDs15M38eG2cYwB1PZpDHScDnDA0=
 -----END RSA PRIVATE KEY-----
 """
 
-rsaTestKeyPublic = b"""
------BEGIN RSA PUBLIC KEY-----
+rsaTestKeyPublic = """-----BEGIN RSA PUBLIC KEY-----
 MIIBCgKCAQEAhAKYdtoeoy8zcAcR874L8cnZxKzAGwd7v36APp7Pv6Q2jdsPBRrw
 WEBnez6d0UDKDwGbc6nxfEXAy5mbhgajzrw3MOEt8uA5txSKobBpKDeBLOsdJKFq
 MGmXCQvEG7YemcxDTRPxAleIAgYYRjTSd/QBwVW9OwNFhekro3RtlinV0a75jfZg
@@ -52,6 +59,35 @@ kne/YiktSvLG34lw2zqXBDTC5NHROUqGTlML4PlNZS5Ri2U4aCNx2rUPRcKIlE0P
 uKxI4T+HIaFpv8+rdV6eUgOrB2xeI1dSFFn/nnv5OoZJEIB+VmuKn3DCUcCZSFlQ
 PSXSfBDiUGhwOw76WuSSsf1D4b/vLoJ10wIDAQAB
 -----END RSA PUBLIC KEY-----
+"""
+
+rsaTestKeyPrivate = """-----BEGIN RSA PRIVATE KEY-----
+MIIEqAIBAAKCAQEAhAKYdtoeoy8zcAcR874L8cnZxKzAGwd7v36APp7Pv6Q2jdsP
+BRrwWEBnez6d0UDKDwGbc6nxfEXAy5mbhgajzrw3MOEt8uA5txSKobBpKDeBLOsd
+JKFqMGmXCQvEG7YemcxDTRPxAleIAgYYRjTSd/QBwVW9OwNFhekro3RtlinV0a75
+jfZgkne/YiktSvLG34lw2zqXBDTC5NHROUqGTlML4PlNZS5Ri2U4aCNx2rUPRcKI
+lE0PuKxI4T+HIaFpv8+rdV6eUgOrB2xeI1dSFFn/nnv5OoZJEIB+VmuKn3DCUcCZ
+SFlQPSXSfBDiUGhwOw76WuSSsf1D4b/vLoJ10wIDAQABAoIBAG/JZuSWdoVHbi56
+vjgCgkjg3lkO1KrO3nrdm6nrgA9P9qaPjxuKoWaKO1cBQlE1pSWp/cKncYgD5WxE
+CpAnRUXG2pG4zdkzCYzAh1i+c34L6oZoHsirK6oNcEnHveydfzJL5934egm6p8DW
++m1RQ70yUt4uRc0YSor+q1LGJvGQHReF0WmJBZHrhz5e63Pq7lE0gIwuBqL8SMaA
+yRXtK+JGxZpImTq+NHvEWWCu09SCq0r838ceQI55SvzmTkwqtC+8AT2zFviMZkKR
+Qo6SPsrqItxZWRty2izawTF0Bf5S2VAx7O+6t3wBsQ1sLptoSgX3QblELY5asI0J
+YFz7LJECgYkAsqeUJmqXE3LP8tYoIjMIAKiTm9o6psPlc8CrLI9CH0UbuaA2JCOM
+cCNq8SyYbTqgnWlB9ZfcAm/cFpA8tYci9m5vYK8HNxQr+8FS3Qo8N9RJ8d0U5Csw
+DzMYfRghAfUGwmlWj5hp1pQzAuhwbOXFtxKHVsMPhz1IBtF9Y8jvgqgYHLbmyiu1
+mwJ5AL0pYF0G7x81prlARURwHo0Yf52kEw1dxpx+JXER7hQRWQki5/NsUEtv+8RT
+qn2m6qte5DXLyn83b1qRscSdnCCwKtKWUug5q2ZbwVOCJCtmRwmnP131lWRYfj67
+B/xJ1ZA6X3GEf4sNReNAtaucPEelgR2nsN0gKQKBiGoqHWbK1qYvBxX2X3kbPDkv
+9C+celgZd2PW7aGYLCHq7nPbmfDV0yHcWjOhXZ8jRMjmANVR/eLQ2EfsRLdW69bn
+f3ZD7JS1fwGnO3exGmHO3HZG+6AvberKYVYNHahNFEw5TsAcQWDLRpkGybBcxqZo
+81YCqlqidwfeO5YtlO7etx1xLyqa2NsCeG9A86UjG+aeNnXEIDk1PDK+EuiThIUa
+/2IxKzJKWl1BKr2d4xAfR0ZnEYuRrbeDQYgTImOlfW6/GuYIxKYgEKCFHFqJATAG
+IxHrq1PDOiSwXd2GmVVYyEmhZnbcp8CxaEMQoevxAta0ssMK3w6UsDtvUvYvF22m
+qQKBiD5GwESzsFPy3Ga0MvZpn3D6EJQLgsnrtUPZx+z2Ep2x0xc5orneB5fGyF1P
+WtP+fG5Q6Dpdz3LRfm+KwBCWFKQjg7uTxcjerhBWEYPmEMKYwTJF5PBG9/ddvHLQ
+EQeNC8fHGg4UXU8mhHnSBt3EA10qQJfRDs15M38eG2cYwB1PZpDHScDnDA0=
+-----END RSA PRIVATE KEY-----
 """
 
 rsaTestKeyPss = str.encode("""
@@ -95,8 +131,7 @@ rOjr9w349JooGXhOxbu8nOxX
 -----END PRIVATE KEY-----
 """)
 
-rsaTestKeyPssPublic = b"""
------BEGIN PUBLIC KEY-----
+rsaTestKeyPssPublic = """-----BEGIN PUBLIC KEY-----
 MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAr4tmm3r20Wd/PbqvP1s2
 +QEtvpuRaV8Yq40gjUR8y2Rjxa6dpG2GXHbPfvMs8ct+Lh1GH45x28Rw3Ry53mm+
 oAXjyQ86OnDkZ5N8lYbggD4O3w6M6pAvLkhk95AndTrifbIFPNU8PPMO7OyrFAHq
@@ -105,6 +140,36 @@ Utzdpiy6LbTgSxP7ociU4Tn0g5I6aDZJ7A8Lzo0KSyZYoA485mqcO0GVAdVw9lq4
 aOT9v6d+nb4bnNkQVklLQ3fVAvJm+xdDOp9LCNCN48V2pnDOkFV6+U9nV5oyc6XI
 2wIDAQAB
 -----END PUBLIC KEY-----
+"""
+
+rsaTestKeyPssPrivate = """-----BEGIN RSA PRIVATE KEY-----
+MIIEvgIBADALBgkqhkiG9w0BAQoEggSqMIIEpgIBAAKCAQEAr4tmm3r20Wd/Pbqv
+P1s2+QEtvpuRaV8Yq40gjUR8y2Rjxa6dpG2GXHbPfvMs8ct+Lh1GH45x28Rw3Ry5
+3mm+oAXjyQ86OnDkZ5N8lYbggD4O3w6M6pAvLkhk95AndTrifbIFPNU8PPMO7Oyr
+FAHqgDsznjPFmTOtCEcN2Z1FpWgchwuYLPL+Wokqltd11nqqzi+bJ9cvSKADYdUA
+AN5WUtzdpiy6LbTgSxP7ociU4Tn0g5I6aDZJ7A8Lzo0KSyZYoA485mqcO0GVAdVw
+9lq4aOT9v6d+nb4bnNkQVklLQ3fVAvJm+xdDOp9LCNCN48V2pnDOkFV6+U9nV5oy
+c6XI2wIDAQABAoIBAQCUB8ip+kJiiZVKF8AqfB/aUP0jTAqOQewK1kKJ/iQCXBCq
+pbo360gvdt05H5VZ/RDVkEgO2k73VSsbulqezKs8RFs2tEmU+JgTI9MeQJPWcP6X
+aKy6LIYs0E2cWgp8GADgoBs8llBq0UhX0KffglIeek3n7Z6Gt4YFge2TAcW2WbN4
+XfK7lupFyo6HHyWRiYHMMARQXLJeOSdTn5aMBP0PO4bQyk5ORxTUSeOciPJUFktQ
+HkvGbym7KryEfwH8Tks0L7WhzyP60PL3xS9FNOJi9m+zztwYIXGDQuKM2GDsITeD
+2mI2oHoPMyAD0wdI7BwSVW18p1h+jgfc4dlexKYRAoGBAOVfuiEiOchGghV5vn5N
+RDNscAFnpHj1QgMr6/UG05RTgmcLfVsI1I4bSkbrIuVKviGGf7atlkROALOG/xRx
+DLadgBEeNyHL5lz6ihQaFJLVQ0u3U4SB67J0YtVO3R6lXcIjBDHuY8SjYJ7Ci6Z6
+vuDcoaEujnlrtUhaMxvSfcUJAoGBAMPsCHXte1uWNAqYad2WdLjPDlKtQJK1diCm
+rqmB2g8QE99hDOHItjDBEdpyFBKOIP+NpVtM2KLhRajjcL9Ph8jrID6XUqikQuVi
+4J9FV2m42jXMuioTT13idAILanYg8D3idvy/3isDVkON0X3UAVKrgMEne0hJpkPL
+FYqgetvDAoGBAKLQ6JZMbSe0pPIJkSamQhsehgL5Rs51iX4m1z7+sYFAJfhvN3Q/
+OGIHDRp6HjMUcxHpHw7U+S1TETxePwKLnLKj6hw8jnX2/nZRgWHzgVcY+sPsReRx
+NJVf+Cfh6yOtznfX00p+JWOXdSY8glSSHJwRAMog+hFGW1AYdt7w80XBAoGBAImR
+NUugqapgaEA8TrFxkJmngXYaAqpA0iYRA7kv3S4QavPBUGtFJHBNULzitydkNtVZ
+3w6hgce0h9YThTo/nKc+OZDZbgfN9s7cQ75x0PQCAO4fx2P91Q+mDzDUVTeG30mE
+t2m3S0dGe47JiJxifV9P3wNBNrZGSIF3mrORBVNDAoGBAI0QKn2Iv7Sgo4T/XjND
+dl2kZTXqGAk8dOhpUiw/HdM3OGWbhHj2NdCzBliOmPyQtAr770GITWvbAI+IRYyF
+S7Fnk6ZVVVHsxjtaHy1uJGFlaZzKR4AGNaUTOJMs6NadzCmGPAxNQQOCqoUjn4XR
+rOjr9w349JooGXhOxbu8nOxX
+-----END RSA PRIVATE KEY-----
 """
 
 requestTarget = "get /foo"
@@ -200,9 +265,12 @@ print(softwrap(sigparamstr))
 print()
 print(softwrap('Signature-Input: sig1=' + str(sigparams)))
 
-key = M2Crypto.RSA.load_key_string(rsaTestKeyPss)
+key = RSA.import_key(PKCS8.unwrap(PEM.decode(rsaTestKeyPssPrivate)[0])[1])
 
-signed = http_sfv.Item(key.sign_rsassa_pss(str.encode(base), algo='sha512'))
+h = SHA512.new(base.encode('utf-8'))
+signer = pss.new(key, mask_func=mgf512, salt_bytes=64)
+
+signed = http_sfv.Item(signer.sign(h))
 
 print("Signed:")
 print(signed)
@@ -214,11 +282,18 @@ print()
 
 # publicKey = M2Crypto.RSA.load_key_string(rsaTestKeyPssPublic)
 
-verified = key.verify_rsassa_pss(str.encode(base), signed.value, algo='sha512')
+pubKey = RSA.import_key(rsaTestKeyPssPublic)
+verifier = pss.new(key, mask_func=mgf512, salt_bytes=64)
 
-print("Verified:")
-print('> YES!' if verified else '> NO!')
-print()
+try:
+    verified = verifier.verify(h, signed.value)
+    print("Verified:")
+    print('> YES!')
+    print()
+except (ValueError, TypeError):
+    print("Verified:")
+    print('> NO!')
+    print()
 
 print('*' * 30)
 
@@ -276,11 +351,12 @@ print(oldsiginput + ', \\')
 print(softwrap('  proxy_sig=' + str(sigparams), 4))
 
 
-key = M2Crypto.RSA.load_key_string(rsaTestKey)
+key = RSA.import_key(rsaTestKeyPrivate)
 
-hashed = hashlib.new('sha256', str.encode(base)).digest()
+h = SHA256.new(base.encode('utf-8'))
+signer = pkcs1_15.new(key)
 
-signed = http_sfv.Item(key.sign(hashed, algo='sha256'))
+signed = http_sfv.Item(signer.sign(h))
 
 print("Signed:")
 print(signed)
@@ -291,18 +367,20 @@ print(oldsig + ', \\')
 print(hardwrap('  proxy_sig=' + str(signed), 4))
 print()
 
-# publicKey = M2Crypto.RSA.load_key_string(rsaTestKeyPublic)
+pubKey = RSA.import_key(rsaTestKeyPublic)
+verifier = pkcs1_15.new(key)
 
-verified = key.verify(hashed, signed.value, algo='sha256')
-
-print("Verified:")
-print('> YES!' if verified else '> NO!')
-print()
-
-
+try:
+    verified = verifier.verify(h, signed.value)
+    print("Verified:")
+    print('> YES!')
+    print()
+except (ValueError, TypeError):
+    print("Verified:")
+    print('> NO!')
+    print()
 
 print('*' * 30)
-
 
 ## minimal signature
 
@@ -328,9 +406,12 @@ print(softwrap(sigparamstr))
 print()
 print(softwrap('Signature-Input: sig1=' + str(sigparams)))
 
-key = M2Crypto.RSA.load_key_string(rsaTestKeyPss)
+key = RSA.import_key(PKCS8.unwrap(PEM.decode(rsaTestKeyPssPrivate)[0])[1])
 
-signed = http_sfv.Item(key.sign_rsassa_pss(str.encode(base), algo='sha512'))
+h = SHA512.new(base.encode('utf-8'))
+signer = pss.new(key, mask_func=mgf512, salt_bytes=64)
+
+signed = http_sfv.Item(signer.sign(h))
 
 print("Signed:")
 print(signed)
@@ -341,12 +422,18 @@ print(hardwrap('Signature: sig1=' + str(signed)))
 
 print()
 
-verified = key.verify_rsassa_pss(str.encode(base), signed.value, algo='sha512')
+pubKey = RSA.import_key(rsaTestKeyPssPublic)
+verifier = pss.new(key, mask_func=mgf512, salt_bytes=64)
 
-print("Verified:")
-print('> YES!' if verified else '> NO!')
-print()
-
+try:
+    verified = verifier.verify(h, signed.value)
+    print("Verified:")
+    print('> YES!')
+    print()
+except (ValueError, TypeError):
+    print("Verified:")
+    print('> NO!')
+    print()
 
 print('*' * 30)
 
@@ -388,11 +475,12 @@ print()
 print(softwrap('Signature-Input: sig1=' + str(sigparams)))
 
 
-key = M2Crypto.RSA.load_key_string(rsaTestKeyPss)
+key = RSA.import_key(PKCS8.unwrap(PEM.decode(rsaTestKeyPssPrivate)[0])[1])
 
-hashed = hashlib.new('sha512', str.encode(base)).digest()
+h = SHA512.new(base.encode('utf-8'))
+signer = pss.new(key, mask_func=mgf512, salt_bytes=64)
 
-signed = http_sfv.Item(key.sign_rsassa_pss(str.encode(base), algo='sha512'))
+signed = http_sfv.Item(signer.sign(h))
 
 print("Signed:")
 print(signed)
@@ -402,14 +490,18 @@ print()
 print(hardwrap('Signature: sig1=' + str(signed)))
 print()
 
-verified = key.verify_rsassa_pss(str.encode(base), signed.value, algo='sha512')
+pubKey = RSA.import_key(rsaTestKeyPssPublic)
+verifier = pss.new(key, mask_func=mgf512, salt_bytes=64)
 
-print("Verified:")
-print('> YES!' if verified else '> NO!')
-print()
-
-
-
+try:
+    verified = verifier.verify(h, signed.value)
+    print("Verified:")
+    print('> YES!')
+    print()
+except (ValueError, TypeError):
+    print("Verified:")
+    print('> NO!')
+    print()
 
 print('*' * 30)
 
@@ -453,10 +545,12 @@ print(softwrap(sigparamstr))
 print()
 print(softwrap('Signature-Input: sig1=' + str(sigparams)))
 
+key = RSA.import_key(PKCS8.unwrap(PEM.decode(rsaTestKeyPssPrivate)[0])[1])
 
-key = M2Crypto.RSA.load_key_string(rsaTestKeyPss)
+h = SHA512.new(base.encode('utf-8'))
+signer = pss.new(key, mask_func=mgf512, salt_bytes=64)
 
-signed = http_sfv.Item(key.sign_rsassa_pss(str.encode(base), algo='sha512'))
+signed = http_sfv.Item(signer.sign(h))
 
 print("Signed:")
 print(signed)
@@ -466,13 +560,17 @@ print()
 print(softwrap('Signature: sig1=' + str(signed)))
 print()
 
-verified = key.verify_rsassa_pss(str.encode(base), signed.value, algo='sha512')
+pubKey = RSA.import_key(rsaTestKeyPssPublic)
+verifier = pss.new(key, mask_func=mgf512, salt_bytes=64)
 
-print("Verified:")
-print('> YES!' if verified else '> NO!')
-print()
-
-
-
+try:
+    verified = verifier.verify(h, signed.value)
+    print("Verified:")
+    print('> YES!')
+    print()
+except (ValueError, TypeError):
+    print("Verified:")
+    print('> NO!')
+    print()
 
 print('*' * 30)
